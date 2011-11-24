4f1f52c1ec23efb413b6251377aba3e4acf63d89
KallDrexx <me@mshapiro.net>
2011-11-24 15:48:10 -0500

    Renamed the in memory user service to MockUserService

--- src/taskdirector/services/mocks/MockUserService.java
+++ src/taskdirector/services/mocks/MockUserService.java
@@ -1,23 +1,27 @@
 
-package taskdirector.services.inmemory;
+package taskdirector.services.mocks;
 
-import java.util.UUID;
+import java.util.*;
 import taskdirector.services.interfaces.IUserService;
 
 /**
  * In Memory implementation of the User Service
  * @author KallDrexx
  */
-public class InMemoryUserService implements IUserService {
-
+public class MockUserService implements IUserService {
+    
+    public MockUserService()
+    {
+    }
+    
     @Override
-    public UUID CreateUser(String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public UUID CreateUser(String username, String password) {     
+        return UUID.randomUUID();
     }
 
     @Override
     public UUID Login(String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return UUID.randomUUID();
     }
 
     @Override
@@ -28,7 +32,5 @@ public class InMemoryUserService implements IUserService {
     @Override
     public void ChangePassword(String username, String currentPassword, String newPassword) {
         throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    
+    }   
 }
